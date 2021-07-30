88bde6faa2ff9c42e7c6c13a47d2c6af2cdae849
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-07-30 12:26:58 +0530

    Random password generate done

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -8,6 +8,7 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
 
     //constructor to receive firstname and lastname
@@ -15,10 +16,17 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED : " + this.firstName + " " + this.lastName);
+
+        //method for ask department
         this.department = setDepartment();
         System.out.println("Department : " + this.department);
+
+        //method for return random password
+        this.password= randomPassword(defaultPasswordLength);
+        System.out.println("Your Password is : " + this.password);
     }
 
+    //Ask department
     private String setDepartment() {
         System.out.println("Department Codes\n1 for sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the Relevant Department Code : ");
         Scanner dept = new Scanner(System.in);
@@ -33,6 +41,17 @@ public class Email {
             return "";
         }
 
+    }
+
+    //generate random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
     }
 
 }
